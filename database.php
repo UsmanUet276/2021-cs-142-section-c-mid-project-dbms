ff70a7ff46d3156180840a46c4aa34d231ca4211
Muhammad Usman Bin Farid <99407553+UsmanUet276@users.noreply.github.com>
2023-02-21 22:43:51 +0500

    FYPProject

---
+++ database.php
@@ -0,0 +1,50 @@
+<?php
+class db {
+    private static $host = "";
+    private static $user = "";
+    private static $password = "";
+    private static $db = "";
+    private static $conn;
+
+    public static function init($host, $user, $password, $db) {
+        self::$host = $host;
+        self::$user = $user;
+        self::$password = $password;
+        self::$db = $db;
+
+        try {
+            self::$conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $password);
+            self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die("Error connecting to database: " . $e->getMessage());
+        }
+    }
+
+    public static function getRecords($query) {
+        $stmt = self::$conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function insertRecords($query) {
+        $stmt = self::$conn->prepare($query);
+        $stmt->execute();
+        return $stmt->rowCount();
+    }
+
+    public static function deleteRecords($query) {
+        $stmt = self::$conn->prepare($query);
+        $stmt->execute();
+        return $stmt->rowCount();
+    }
+
+    public static function updateRecords($query) {
+        $stmt = self::$conn->prepare($query);
+        $stmt->execute();
+        return $stmt->rowCount();
+    }
+}
+
+// Connect to database automatically when the file is included
+db::init("DESKTOP-OFGEF8P", "", "", "ProjectA");
+?>
---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+    include("header.php");
+    include("database.php");
+    $query = "SELECT COUNT(*) FROM Student";
+    $res = db::getRecords($query);
+    $stu = $res[0][''];
+
+    $query = "SELECT COUNT(*) FROM [Group]";
+    $res = db::getRecords($query);
+    $gro = $res[0][''];
+
+    $query = "SELECT COUNT(*) FROM Project";
+    $res = db::getRecords($query);
+    $pro = $res[0][''];
+
+    $query = "SELECT COUNT(*) FROM Advisor";
+    $res = db::getRecords($query);
+    $adv = $res[0][''];
+?>
+        <div class="sa4d25">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h2 class="st_title"><i class="uil uil-apps"></i> User Dashboard</h2>
+                    </div>
+                    <div class="col-xl-3 col-lg-6 col-md-6">
+                        <div class="card_dash">
+                            <div class="card_dash_left">
+                                <h5>Total Students</h5>
+                                <h2><?php echo $stu;?></h2>
+                            </div>
+                            <div class="card_dash_right">
+                                <img src="images/dashboard/achievement.svg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-6 col-md-6">
+                        <div class="card_dash">
+                            <div class="card_dash_left">
+                                <h5>Total Groups</h5>
+                                <h2><?php echo $gro;?></h2>
+                            </div>
+                            <div class="card_dash_right">
+                                <img src="images/dashboard/graduation-cap.svg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-6 col-md-6">
+                        <div class="card_dash">
+                            <div class="card_dash_left">
+                                <h5>Total Projects</h5>
+                                <h2><?php echo $pro;?></h2>
+                            </div>
+                            <div class="card_dash_right">
+                                <img src="images/dashboard/online-course.svg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-6 col-md-6">
+                        <div class="card_dash">
+                            <div class="card_dash_left">
+                                <h5>Total Advisors</h5>
+                                <h2><?php echo $adv;?></h2>
+                            </div>
+                            <div class="card_dash_right">
+                                <img src="images/dashboard/knowledge.svg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="card_dash1">
+                            <div class="card_dash_left1">
+                                <i class="uil uil-book-alt"></i>
+                                <h1>Jump Into Course Creation</h1>
+                            </div>
+                            <div class="card_dash_right1">
+                                <button class="create_btn_dash"
+                                    onclick="window.location.href = 'create_new_course.html';">Create Your
+                                    Course</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+<?php
+    include("footer.php");
+?>
