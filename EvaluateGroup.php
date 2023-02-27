8690873611406c097f02f1f88cbfc53c51a663af
Muhammad Usman Bin Farid <99407553+UsmanUet276@users.noreply.github.com>
2023-02-27 18:34:44 +0500

    Push existing project to GitLab

---
+++ EvaluateGroup.php
@@ -0,0 +1,90 @@
+<?php
+    include("header.php");
+    include("database.php");
+    $query = "SELECT * FROM [Group]";
+    $res1 = db::getRecords($query);
+    $query = "SELECT * FROM Evaluation";
+    $res = db::getRecords($query);
+?>
+       <div class="sa4d25">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        
+                        <div class="tab-content" id="pills-tabContent">
+                            <div class="tab-pane fade show active" id="pills-account" role="tabpanel"
+                                aria-labelledby="pills-account-tab">
+                                <div class="account_setting">
+                                    <div class="basic_profile">
+                                        <div class="basic_ptitle">
+                                            <h4>Evaluate Group</h4>
+                                        </div>
+                                        <form method="POST" action ="action.php">
+                                        <div class="basic_form">
+                                            <div class="row">
+                                                <div class="col-lg-8">
+                                                    <div class="row">
+                                                        <div class="col-lg-12">
+                                                            <div class="ui search focus mt-30">
+                                                                <div class="ui left icon input swdh11 swdh19">
+                                                                    <select class="ui hj145 dropdown cntry152 prompt srch_explore" name="gen">
+                                                                    <option value="">Select Group Id</option>
+                                                                            <?php 
+                                                                            foreach($res1 as $pro)
+                                                                            {
+                                                                            
+                                                                            echo '<option value="'. $pro['Id'].'">'.$pro['Id'] .'</option>';
+                                                                            }
+                                                                            ?>
+                                                                    </select>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-lg-12">
+                                                            <div class="ui search focus mt-30">
+                                                                <div class="ui left icon input swdh11 swdh19">
+                                                                    <select class="ui hj145 dropdown cntry152 prompt srch_explore" name="gen">
+                                                                    <option value="">Select Evaluation Name</option>
+                                                                            <?php 
+                                                                            foreach($res as $pro)
+                                                                            {
+                                                                            
+                                                                            echo '<option value="'. $pro['Id'].'">'.$pro['Name'] .'</option>';
+                                                                            }
+                                                                            ?>
+                                                                    </select>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-lg-12">
+                                                            <div class="ui search focus mt-30">
+                                                                <div class="ui left icon input swdh11 swdh19">
+                                                                    <input class="prompt srch_explore" type="text"
+                                                                        name="roll"  id="id[surname]"
+                                                                        required="" maxlength="64"
+                                                                        placeholder="Obtained Marks">
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-lg-12">
+                                                            <div class="divider-1"></div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        
+                                    <button class="save_btn" type="submit" name="addpro">Submit</button>
+                                    <form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     
+<?php
+    include("footer.php");
+?>
